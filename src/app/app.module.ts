0b3cc6b0e78e7a1f3a418d1e4313295186ee2b74
Benjamin Greene <bengreene@Conrad.lan>
2018-09-20 22:33:23 +0100

    Add booking and home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookingComponent } from './booking/booking.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookingComponent,
+    HomeComponent,
   ],
-  imports: [],
+  imports: [
+    // AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
