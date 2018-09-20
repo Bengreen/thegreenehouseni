e8e3dd9d3b4ec227566f61571ef33471017259d8
Benjamin Greene <bengreene@Conrad.lan>
2018-09-20 21:20:01 +0100

    add Ivy Render

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { enableProdMode } from '@angular/core';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// 
+// import { AppModule } from './app/app.module';
+// import { environment } from './environments/environment';
+// 
+// if (environment.production) {
+//   enableProdMode();
+// }
+// 
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { ɵrenderComponent as renderComponent } from '@angular/core';
 
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+renderComponent(AppComponent);
 
