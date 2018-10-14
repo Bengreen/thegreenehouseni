dc607daa2dcaffdb55ab3f13371d991e628d43fa
Benjamin Greene <bengreene@Conrad.lan>
2018-10-14 13:22:21 +0100

    Add sections for each area. Add menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BookingComponent } from './booking/booking.component';
 import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ThingsToDoComponent } from './things-to-do/things-to-do.component';
+import { DirectionsComponent } from './directions/directions.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -12,6 +15,18 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'thingstodo',
+    component: ThingsToDoComponent,
+  },
+  {
+    path: 'aboutus',
+    component: AboutUsComponent,
+  },
+  {
+    path: 'directions',
+    component: DirectionsComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,27 @@ import { AppComponent } from './app.component';
 import { BookingComponent } from './booking/booking.component';
 import { HomeComponent } from './home/home.component';
 
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { AlertModule } from 'ngx-bootstrap';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ThingsToDoComponent } from './things-to-do/things-to-do.component';
+import { DirectionsComponent } from './directions/directions.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     BookingComponent,
     HomeComponent,
+    AboutUsComponent,
+    ThingsToDoComponent,
+    DirectionsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BsDropdownModule.forRoot(),
+    AlertModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
