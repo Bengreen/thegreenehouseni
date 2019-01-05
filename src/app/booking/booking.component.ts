394b6e7bbd1ab77d8186c10e9a8737fc1f7f8a29
Benjamin Greene <bengreene@Conrad.lan>
2019-01-05 01:01:27 +0000

    Add some content in for things to do and placemarkers for all house photos.

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -11,5 +11,21 @@ export class BookingComponent implements OnInit {
 
   ngOnInit() {
   }
+  cards=[
+    {
+      title: "Book Directly with our Agent",
+      description: 'Please call Robert on <a href="t:+447729960546">+44 7729 960 546</a> to book',
+    },
+    {
+      title: "AirBnB",
+      image: "https://upload.wikimedia.org/wikipedia/commons/6/69/Airbnb_Logo_B%C3%A9lo.svg",
+      description: 'Book with <a href="https://www.airbnb.co.uk/rooms/29093261">AirBnB</a>',
+    },
+    {
+      title: "Booking.com",
+      image: "https://upload.wikimedia.org/wikipedia/en/thumb/b/be/Booking.com_logo.svg/1280px-Booking.com_logo.svg.png",
+      description: 'Book with <a href="https://www.booking.com/hotel/gb/the-greene-house.en-gb.html">Booking.com</a>',
+    },
 
+  ]
 }
--- src/app/things-to-do/things-to-do.component.ts
+++ src/app/things-to-do/things-to-do.component.ts
@@ -20,47 +20,41 @@ export class ThingsToDoComponent implements OnInit {
       image: "assets/images/image_thmb.png",
     },
     {
-      title: "Walk Ballykelly Bank2",
-      subtitle: "",
+      title: "Visit Samsons Tower",
       description: 'Walk along <a href="http://www.walkni.com/walks/164/ballykelly-bank/">Ballykelly Bank</a> using a <a href="http://www.walkni.com/d/walks/164/Ballykelly_Bank.pdf">WalkNI Map</a>',
-      image: "assets/brown.png",
     },
     {
-      title: "Walk Ballykelly Bank2",
-      subtitle: "",
+      title: "Cycle the Ballykelly Bank",
       description: 'Walk along <a href="http://www.walkni.com/walks/164/ballykelly-bank/">Ballykelly Bank</a> using a <a href="http://www.walkni.com/d/walks/164/Ballykelly_Bank.pdf">WalkNI Map</a>',
-      image: "assets/brown.png",
     },
     {
-      title: "Walk Ballykelly Bank2",
-      subtitle: "",
+      title: "Visit The Diamond",
       description: 'Walk along <a href="http://www.walkni.com/walks/164/ballykelly-bank/">Ballykelly Bank</a> using a <a href="http://www.walkni.com/d/walks/164/Ballykelly_Bank.pdf">WalkNI Map</a>',
-      image: "assets/brown.png",
     },
     {
-      title: "Walk Ballykelly Bank2",
-      subtitle: "",
+      title: "Visit Ballykelly Triangulation Tower",
       description: 'Walk along <a href="http://www.walkni.com/walks/164/ballykelly-bank/">Ballykelly Bank</a> using a <a href="http://www.walkni.com/d/walks/164/Ballykelly_Bank.pdf">WalkNI Map</a>',
-      image: "assets/brown.png",
     },
     {
-      title: "Walk Ballykelly Bank2",
-      subtitle: "",
+      title: "Jog around Ballykelly Forrest",
       description: 'Walk along <a href="http://www.walkni.com/walks/164/ballykelly-bank/">Ballykelly Bank</a> using a <a href="http://www.walkni.com/d/walks/164/Ballykelly_Bank.pdf">WalkNI Map</a>',
-      image: "assets/brown.png",
     },
     {
-      title: "Walk Ballykelly Bank2",
-      subtitle: "",
+      title: "Visit Roe Vally Country Park",
       description: 'Walk along <a href="http://www.walkni.com/walks/164/ballykelly-bank/">Ballykelly Bank</a> using a <a href="http://www.walkni.com/d/walks/164/Ballykelly_Bank.pdf">WalkNI Map</a>',
-      image: "assets/brown.png",
     },
     {
-      title: "Walk Ballykelly Bank2",
-      subtitle: "",
+      title: "Kidz Ahoy at Roe Valley Leisure Centre",
       description: 'Walk along <a href="http://www.walkni.com/walks/164/ballykelly-bank/">Ballykelly Bank</a> using a <a href="http://www.walkni.com/d/walks/164/Ballykelly_Bank.pdf">WalkNI Map</a>',
-      image: "assets/brown.png",
     },
+    {
+      title: "Visit the Tower Museum",
+    },
+    {
+      title: "Cycle at Ballycarton Wood",
+      description: 'Cycle the Binevenagh Trail in <a href="http://www.cycleni.com/64/ballycarton-woodbinevenagh-trail/">Ballycarton Wood</a>',
+
+    }
   ]
 
 }
