ab338f9560ed763d25be600e8e73867438d3592a
Ben Greene <Ben.Greene@digitalreasoning.com>
2019-12-27 22:30:39 +0000

    Fix browser animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,6 +27,7 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     BsDropdownModule.forRoot(),
     AlertModule.forRoot(),
